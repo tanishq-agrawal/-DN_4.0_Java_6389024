04a97d947dd4e8654a5b6286567455823fc4c0b3
Tanishq <tanishqagrawal1811@gmail.com>
2025-07-20 02:09:58 +0530

    Adding week 5 hands-on

---
+++ Week_5/5_Microservices_with_Spring_Boot_3_and_Spring_Cloud/1_Microservices/2_Microservices_with_API_gateway/Additional/Create_Eureka_Discovery_Server_and_register/Code/loans/src/main/java/com/cognizant/loans/LoansApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.loans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LoansApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoansApplication.class, args);
+    }
+}
